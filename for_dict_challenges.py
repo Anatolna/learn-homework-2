6213d36438e9bf7d9104879b45b6f856b653167d
Anatolna <taotoj@gmail.com>
2019-10-01 14:24:53 +0300

    updated for_dict_challenges

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -1,8 +1,9 @@
+
 # Задание 1
 # Дан список учеников, нужно посчитать количество повторений каждого имени ученика.
 print('Задание 1')
 import collections
-from collections import Counter
+#from collections import Counter
 
 students = [
   {'first_name': 'Вася'},
@@ -10,14 +11,14 @@ students = [
   {'first_name': 'Маша'},
   {'first_name': 'Маша'},
   {'first_name': 'Петя'},
+  {'first_name': 'Петя'},
+  {'first_name': 'Петя'},
 ]
-all_names = []
-for name in students:
-    all_names.append(name['first_name'])
-    #print(', '.join(all_names))
-count = Counter(all_names)
+all_names = [name['first_name'] for name in students]
+count = collections.Counter(all_names)
 for nn in count:
-  print(f'{nn}:', count[nn])
+  #print(f'{nn}:', count[nn])
+  print(nn, count[nn])
 
 
 # Пример вывода:
@@ -35,28 +36,16 @@ students = [
   {'first_name': 'Петя'},
   {'first_name': 'Маша'},
   {'first_name': 'Маша'},
-  {'first_name': 'Оля'},
-  {'first_name': 'Оля'},
-  {'first_name': 'Оля'},
+  {'first_name': 'Миша'},
 ]
-one_list = []
-for person in students:
-  one_list.append(person['first_name'])
-
+one_list = [person['first_name'] for person in students]
 most_common = None
-qty_most_common = 0
-
 for name in one_list:
-    qty = one_list.count(name)
-    #print(qty)
-    if qty > qty_most_common:
-        qty_most_common = qty
-        most_common = name
+    most_common = max(set(one_list), key=one_list.count)
 print('Самое частое имя среди учеников:', most_common)
 
 # Пример вывода:
 # Самое частое имя среди учеников: Маша
-
 
 # Задание 3
 # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
@@ -75,33 +64,22 @@ school_students = [
   ],
   [ # это - третий класс
     {'first_name': 'Сеня'},
-    {'first_name': 'Сеня'},
+    {'first_name': 'Саня'},
     {'first_name': 'Саня'},
   ]
 ]
 # Пример вывода:
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
-one_class = []
 n = 0
 for one_class in school_students:
   n += 1
   #print(f'класс номер {n}', one_class)
-  klass = []
-  for names in one_class:
-    klass.append(names['first_name'])
-    #print(f'имена класса {n}', ', '.join(klass))
-
+  each_klass = [person['first_name'] for person in one_class]
+  #print(each_klass)
   most_common = None
-  qty_most_common = 0
-
-  for person in klass:
-    #print(f'имя ученика класса {n}:', person)
-    qty = klass.count(person)
-    #print(f'число {person} в классе {n}', qty)
-    if qty > qty_most_common:
-        qty_most_common = qty
-        most_common = person
+  for person in one_class:
+    most_common = max(set(each_klass), key=each_klass.count)
   print(f'Самое частое имя в классе {n}: ', most_common)
 
 # Задание 4
@@ -121,24 +99,25 @@ is_male = {
 # Пример вывода:
 # В классе 2a 2 девочки и 0 мальчика.
 # В классе 3c 0 девочки и 2 мальчика.
-
+#def split_list():
 one_class = []
 for klass in school:
   one_class = klass['students']
+  #return one_class
   #print(one_class)
   persons = []
-  m = 0
-  f = 0
+  b = 0
+  g = 0
   for name in one_class:
     persons = name['first_name']
     #print(persons)
     if is_male[persons] == True:
       #print(f'{persons} - мальчик')
-      m += 1
+      b += 1
     else:
       #print(f'{persons} - девочка')
-      f += 1
-  print(f'В классе {klass["class"]} {f} девочки и {m} мальчика')
+      g += 1
+  print(f'В классе {klass["class"]} {g} девочки и {b} мальчика')
 
   # Задание 5
 # По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков.
@@ -157,22 +136,20 @@ is_male = {
 # Пример вывода:
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
-pupils = []
+one_class = []
 boys = 0
 girls = 0
 male_klass = None
 fem_klass = None
 for klass in school:
-  pupils = klass['students']
-  #print(pupils)
-  names = []
+  one_class = klass['students']
+  #print(one_class)
+  persons = []
   m = 0
   f = 0
-  for person in pupils:
-    names = person['first_name']
-    #print(person)
-    #print(names)
-    if is_male[names] == True:
+  for name in one_class:
+    persons = name['first_name']
+    if is_male[persons] == True:
       m += 1
     else:
       f += 1
